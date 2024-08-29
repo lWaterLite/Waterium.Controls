5896f5791ac0a8e786ca2673ba8787b6b476ca69
lWaterLite <lwaterlite1@outlook.com>
2024-08-29 19:26:49 +0800

    feat(ResizableWindow): Complete ResizableWindow.

---
+++ Waterium.Controls.Window/ResizableWindow.cs
@@ -0,0 +1,71 @@
+﻿using System.Data;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
+
+namespace Waterium.Controls.Window;
+
+/// <summary>
+/// Follow steps 1a or 1b and then 2 to use this custom control in a XAML file.
+///
+/// Step 1a) Using this custom control in a XAML file that exists in the current project.
+/// Add this XmlNamespace attribute to the root element of the markup file where it is 
+/// to be used:
+///
+///     xmlns:MyNamespace="clr-namespace:Waterium.Controls.Window"
+///
+///
+/// Step 1b) Using this custom control in a XAML file that exists in a different project.
+/// Add this XmlNamespace attribute to the root element of the markup file where it is 
+/// to be used:
+///
+///     xmlns:MyNamespace="clr-namespace:Waterium.Controls.Window;assembly=Waterium.Controls.Window"
+///
+/// You will also need to add a project reference from the project where the XAML file lives
+/// to this project and Rebuild to avoid compilation errors:
+///
+///     Right click on the target project in the Solution Explorer and
+///     "Add Reference"->"Projects"->[Select this project]
+///
+///
+/// Step 2)
+/// Go ahead and use your control in the XAML file.
+///
+///     <MyNamespace:ResizableWindow/>
+///
+/// </summary>
+public class ResizableWindow : System.Windows.Window
+{
+    static ResizableWindow()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(typeof(ResizableWindow),
+            new FrameworkPropertyMetadata(typeof(ResizableWindow)));
+    }
+
+    protected ResizableWindow()
+    {
+        CommandBindings.Add(new CommandBinding(SystemCommands.CloseWindowCommand,
+            (_, _) => SystemCommands.CloseWindow(this)));
+        CommandBindings.Add(new CommandBinding(SystemCommands.MinimizeWindowCommand,
+            (_, _) => SystemCommands.MinimizeWindow(this)));
+        CommandBindings.Add(new CommandBinding(SystemCommands.MaximizeWindowCommand,
+            (_, _) => SystemCommands.MaximizeWindow(this)));
+        CommandBindings.Add(new CommandBinding(SystemCommands.RestoreWindowCommand,
+            (_, _) => SystemCommands.RestoreWindow(this)));
+    }
+}
+
+public class IconToVisibilityConverter : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return value == null ? Visibility.Collapsed : Visibility.Visible;
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new EvaluateException("Convert Icon to Visibility is not allowed.");
+    }
+}
