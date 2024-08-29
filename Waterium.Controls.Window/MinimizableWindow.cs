4268ab51194654af292923a9cc85e8af4448e84f
lWaterLite <lwaterlite1@outlook.com>
2024-08-29 19:36:12 +0800

    feat(MinimizableWindow): Complete MinimizableWindow.

---
+++ Waterium.Controls.Window/MinimizableWindow.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Waterium.Controls.Window;
+
+public class MinimizableWindow : System.Windows.Window
+{
+    static MinimizableWindow()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(typeof(MinimizableWindow),
+            new FrameworkPropertyMetadata(typeof(MinimizableWindow)));
+    }
+
+    protected MinimizableWindow()
+    {
+        CommandBindings.Add(new CommandBinding(SystemCommands.CloseWindowCommand,
+            (_, _) => SystemCommands.CloseWindow(this)));
+        CommandBindings.Add(new CommandBinding(SystemCommands.MinimizeWindowCommand,
+            (_, _) => SystemCommands.MinimizeWindow(this)));
+    }
+}
